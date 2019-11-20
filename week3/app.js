0403cd166b7d058d324de3610b592e122d5f82dc
Zeke Lin <zeke5252@yahoo.com.tw>
2019-11-20 19:40:19 +0800

    Update gitignore & assignment3-2

--- week3/app.js
+++ week3/app.js
@@ -1,60 +1,42 @@
-// Assignment 1
-
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieParser = require("cookie-parser");
 
 const app = express();
 
-
-
-const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-
-// Assignment 3
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false}));
+app.use(cookieParser());
 
 app.set('view engine', 'pug');
-
 let total;
 
 app.get('/', (req, res) => {
     res.render('index');
 });
 
-app.get('/sum', (req, res) => {
-
-    let xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function(){
-        if(xhr.readyState === 4 && xhr.status === 200){
-           // console.log('haha');
-        }
-    };
-    xhr.open('GET','http://localhost:3000/getData?number=10');
-    xhr.send();
-    res.render('sum', {total});
+app.get('/sum.html', (req, res) => {
+    let cookieNum = req.cookies.yourNum;
+    res.send({total, cookieNum});
 });
 
-// Assignment 2
-
 app.get('/getData', (req, res) => {
-
-    total= 0;
+    total = 0;
     let arr = req.query.number;
     for(let i=0; i<=arr; i++){
         total+=i;
     }
-    res.render('getData', {total});
+    res.render('getData', {total, arr});
 });
 
 app.post('/getData', (req, res) => {
-//I should build a route of getData, otherwise I can't access it in this function @ @
     total2= 0;
     let arr = req.body.yourNumber;
     for(let i=0; i<=arr; i++){
         total2+=i;
     }
-console.log('haha')
-    res.render('sum',{resultNum: total2});
+    res.cookie('yourNum',arr)
+    res.send({total2});
 });
 
 app.listen(3000, () => {
