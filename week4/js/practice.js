d3dc5d43019e365471cb62ccad51134c9d65ee04
Zeke Lin <zeke5252@yahoo.com.tw>
2019-11-27 15:29:00 +0800

    Week4 assignment

---
+++ week4/js/practice.js
@@ -0,0 +1,103 @@
+//test1
+
+// function max(...numbers){ 
+//     const arr = [];
+//     let largest = numbers.reduce( (prevVal, curVal) => {
+//         return ( prevVal > curVal ) ? prevVal:curVal;
+//         })
+//         console.log(largest);
+// } 
+
+// max(1, 2, 4, 5);  // result to 5 
+
+// max(5, 2, 7, 1, 6);  // result to 7
+
+
+
+
+
+//test2
+
+
+
+
+// Try to call calculate function correctly 
+/* For example, if we have an add function like this: 
+function add(args){ return args.n1+args.n2; } 
+We can call it by passing an object created by JSON literal: 
+add({n1:3, n2:4}); // your first way 
+You should find another way to create a proper object.  
+// your second way */ 
+
+// function calculate(args){
+//     let result; 
+//     if(args.op==="+"){ 
+//         result=args.n1+args.n2; 
+//        }else if(args.op==="-"){ 
+//            result=args.n1-args.n2; 
+//        }else{ 
+//            result="Not supported"; 
+//        } return result; 
+//    } 
+
+//first way
+// function createObj(op, n1, n2) {
+//     let obj={};
+//     obj.op = op;
+//     obj.n1 = n1;
+//     obj.n2 = n2;
+// return obj;
+// }
+// console.log('first way > ', calculate(createObj('+', 4, 5)));
+
+//second way
+// class CreateObj2 {
+//     constructor(op, n1, n2){
+//         this.op = op;
+//         this.n1 = n1;
+//         this.n2 = n2;
+//     }
+// }
+// let newObj = new CreateObj2('+', 4, 5) 
+// console.log('second way > ', calculate(newObj));
+
+
+
+//test3
+
+// function avg(data){
+//     let sum = 0;
+//     for(product of data.products){
+//         sum += product.price;
+//     }
+//     console.log(Math.floor(sum/data.size));
+//     return Math.floor(sum/data.size);
+
+// } 
+
+// avg({ 
+//     size:3, 
+//     products:[
+//          { name:"Product 1", price:100 }, 
+//          { name:"Product 2", price:700 }, 
+//          { name:"Product 3", price:250 } 
+//         ] 
+//     }); // show the average price of all products
+//函式裡面帶入的參數是一個物件，有size, products等key，size是個數值；products是個陣列，現在要計算所有產品的均價
+//加總所有產品價格，除以產品數量
+
+
+
+//Week4 - assignment 1
+
+function delayedResult(n1, n2, delayTime, callback){
+    let addNum = (n1, n2) => n1 + n2;
+    let result = addNum(n1, n2);
+    window.setTimeout(callback.bind(null, result), delayTime);
+    } 
+delayedResult(4, 5, 3000, function(result){
+     console.log(result); 
+    });
+delayedResult(-5, 10, 9000, function(result){
+     window.alert(result); }
+     );
---
+++ week4/js/web.js
@@ -0,0 +1,145 @@
+class Slogan extends React.Component {
+    state = {
+        mySlogan : 'Helllllo~~~~This is my website'
+    }
+    changeText = () => {
+        this.setState({
+            mySlogan: 'Have a Good Time!'
+        });
+    };
+    render(){
+        return (
+            <div className="header-slogan">
+                    <div className="header-text" id="header-welcome" onClick={this.changeText}>
+                        {this.state.mySlogan}
+                    </div>
+            </div>
+        )
+    }
+};
+class BoxContainer extends React.Component {
+    render(){
+        return (
+            <div className="row">
+                <div className="col-1-of-2">
+                    <div className="main-box">
+                        Content box {this.props.boxNumber[0]} 
+                    </div>
+                </div>
+                <div className="col-1-of-2">
+                    <div className="main-box">
+                        Content box {this.props.boxNumber[1]}  
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+class MainContainer extends React.Component {
+    state = {
+        idRowHidden: ['.row:nth-child(4)','.row:nth-child(5)','.row:nth-child(6)']
+    }
+    showBoxes = () => {
+        for(let i=0; i<this.state.idRowHidden.length; i++){
+            document.querySelector(this.state.idRowHidden[i]).style.display='block';
+        }
+    };
+    render(){
+        return (
+            <section className="main-content">
+                <h2 className="main-title">{this.props.secTitle}</h2>
+                <BoxContainer boxNumber={[1,2]} />
+                <BoxContainer boxNumber={[3,4]} />
+                <BoxContainer boxNumber={[5,6]} />
+                <BoxContainer boxNumber={[7,8]} />
+                <BoxContainer boxNumber={[9,10]} />
+                <a href="#" className="main-button" onClick={this.showBoxes}>
+                Call to action
+                </a>
+            </section>
+        )
+    }
+}
+class Menu extends React.Component {
+    state = {
+        idHeaderNavMobile: '.header-nav-mobile'
+    }
+    switchMenu = () => {
+        let tempStr = document.querySelector(this.state.idHeaderNavMobile).classList;
+        (tempStr.contains('displayBlock'))? tempStr.remove('displayBlock'):tempStr.add('displayBlock');
+    };
+    render(){
+        return(
+            <img src="img/menu.svg" alt="" className="header-menu" onClick = {this.switchMenu}/>
+        )
+    }
+}
+class Close extends React.Component {
+    state = {
+        idHeaderNavMobile: '.header-nav-mobile'
+    }
+    switchMenu = () => {
+        let tempStr = document.querySelector(this.state.idHeaderNavMobile).classList;
+        (tempStr.contains('displayBlock'))? tempStr.remove('displayBlock'):tempStr.add('displayBlock');
+    };
+    render(){
+        return(
+            <img src="img/close.svg" alt="close" title="icon designed by Zeke" className="header-closeImg" onClick = {this.switchMenu}/>
+        )
+    }
+}
+class Navlist extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render(){
+        return(
+            <ul className={this.props.class}>
+                <li>About</li>
+                <li>Works</li>
+                <li>Experiences</li>
+                <li>Contact me</li>
+            </ul>
+        )
+    }
+}
+class Container extends React.Component {
+    render(){
+        return(
+            <div className="container">
+                <header>
+                    <header className="header-head">
+                        <div className="header-logo">
+                            <span className="header-title">Zeke's website</span>
+                            <img className="header-logoImg" src="img/logo.svg"  />
+                        </div>
+                        <Menu />
+                        <nav className="header-nav-mobile displaySwitch">
+                            <Close />
+                            <Navlist class="header-ul-mobile" />
+                        </nav>
+                        <nav className="header-nav">
+                            <Navlist class="header-ul" />
+                        </nav>
+                    </header>
+                    <Slogan />
+                </header>
+                <MainContainer secTitle="Here is section title" />
+            </div>
+        )
+    }
+};
+ReactDOM.render(
+    <Container />,
+    document.getElementById('root')
+)
+//Pending issue. The width is separated with that in CSS, which means I need to modify both files if I change the width
+window.addEventListener("resize", function() {
+    let tempStr = document.querySelector('.header-nav-mobile').classList;
+    if (window.matchMedia("(min-width: 800px)").matches) {
+    //Remove the class
+    tempStr.remove('displayBlock')
+    } else {
+    console.log("stay as it is");
+    }
+    });
