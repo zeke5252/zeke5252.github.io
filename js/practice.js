9d32f8cafd920ed718f59dd50445bb897dc8cf17
Zeke Lin <zeke5252@yahoo.com.tw>
2019-11-27 15:40:22 +0800

    week4

--- js/practice.js
+++ js/practice.js
@@ -29,37 +29,37 @@ add({n1:3, n2:4}); // your first way
 You should find another way to create a proper object.  
 // your second way */ 
 
-function calculate(args){
-    let result; 
-    if(args.op==="+"){ 
-        result=args.n1+args.n2; 
-       }else if(args.op==="-"){ 
-           result=args.n1-args.n2; 
-       }else{ 
-           result="Not supported"; 
-       } return result; 
-   } 
+// function calculate(args){
+//     let result; 
+//     if(args.op==="+"){ 
+//         result=args.n1+args.n2; 
+//        }else if(args.op==="-"){ 
+//            result=args.n1-args.n2; 
+//        }else{ 
+//            result="Not supported"; 
+//        } return result; 
+//    } 
 
 //first way
-function createObj(op, n1, n2) {
-    let obj={};
-    obj.op = op;
-    obj.n1 = n1;
-    obj.n2 = n2;
-return obj;
-}
-console.log('first way > ', calculate(createObj('+', 4, 5)));
+// function createObj(op, n1, n2) {
+//     let obj={};
+//     obj.op = op;
+//     obj.n1 = n1;
+//     obj.n2 = n2;
+// return obj;
+// }
+// console.log('first way > ', calculate(createObj('+', 4, 5)));
 
 //second way
-class CreateObj2 {
-    constructor(op, n1, n2){
-        this.op = op;
-        this.n1 = n1;
-        this.n2 = n2;
-    }
-}
-let newObj = new CreateObj2('+', 4, 5) 
-console.log('second way > ', calculate(newObj));
+// class CreateObj2 {
+//     constructor(op, n1, n2){
+//         this.op = op;
+//         this.n1 = n1;
+//         this.n2 = n2;
+//     }
+// }
+// let newObj = new CreateObj2('+', 4, 5) 
+// console.log('second way > ', calculate(newObj));
 
 
 
@@ -85,3 +85,19 @@ console.log('second way > ', calculate(newObj));
 //     }); // show the average price of all products
 //函式裡面帶入的參數是一個物件，有size, products等key，size是個數值；products是個陣列，現在要計算所有產品的均價
 //加總所有產品價格，除以產品數量
+
+
+
+//Week4 - assignment 1
+
+function delayedResult(n1, n2, delayTime, callback){
+    let addNum = (n1, n2) => n1 + n2;
+    let result = addNum(n1, n2);
+    window.setTimeout(callback.bind(null, result), delayTime);
+    } 
+delayedResult(4, 5, 3000, function(result){
+     console.log(result); 
+    });
+delayedResult(-5, 10, 9000, function(result){
+     window.alert(result); }
+     );
--- js/web.js
+++ js/web.js
@@ -1,51 +1,143 @@
-//request1
-//Get the DOM - welcome
-//Add listener to this DOM，and callback function
-//This callback function is in charge of the replacement of the text.
-//Wrap the entire functions ( IIFE )
-
-
-//request2
-//1.Click the menu btn, display the hidden mobile menu
-//2.Click the close btn hide the mobile menu.
-//3.if the header width is larger than 800, hide the mobile menu, (Already: display the horizontal one) 
-//Finanlly, style the nav.
-
-//request3
-//Get the position of the content boxes.
-//If the y position of the rows plus its height surpasses current window's y, hide these boxes, and display the call to action btn.  
-
-const DOM = {
-    welcome: '#header-welcome',
-    menuImg: '.header-menu',
-    menuMobile: '.header-nav-mobile',
-    closeMobile: '.header-closeImg',
-    btnMain: '.main-button',
-    rowHidden: ['.row:nth-child(4)','.row:nth-child(5)','.row:nth-child(6)']
-};
-function changeText(){
-    this.innerHTML = 'Have a Good Time!';
-};
-function switchMenu(){
-    let tempStr = document.querySelector(DOM.menuMobile).classList;
-    (tempStr.contains('displayBlock'))? tempStr.remove('displayBlock'):tempStr.add('displayBlock');
-};
-
-function showBoxes(){
-    for (child of DOM.rowHidden){
-        console.log(child);
-        document.querySelector(child).style.display='block';
+class Slogan extends React.Component {
+    state = {
+        mySlogan : 'Helllllo~~~~This is my website'
+    }
+    changeText = () => {
+        this.setState({
+            mySlogan: 'Have a Good Time!'
+        });
+    };
+    render(){
+        return (
+            <div className="header-slogan">
+                    <div className="header-text" id="header-welcome" onClick={this.changeText}>
+                        {this.state.mySlogan}
+                    </div>
+            </div>
+        )
     }
 };
-
-document.querySelector(DOM.welcome).addEventListener('click', changeText);
-document.querySelector(DOM.menuImg).addEventListener('click', switchMenu);
-document.querySelector(DOM.closeMobile).addEventListener('click', switchMenu);
-document.querySelector(DOM.btnMain).addEventListener('click', showBoxes);
-
+class BoxContainer extends React.Component {
+    render(){
+        return (
+            <div className="row">
+                <div className="col-1-of-2">
+                    <div className="main-box">
+                        Content box {this.props.boxNumber[0]} 
+                    </div>
+                </div>
+                <div className="col-1-of-2">
+                    <div className="main-box">
+                        Content box {this.props.boxNumber[1]}  
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+class MainContainer extends React.Component {
+    state = {
+        idRowHidden: ['.row:nth-child(4)','.row:nth-child(5)','.row:nth-child(6)']
+    }
+    showBoxes = () => {
+        for(let i=0; i<this.state.idRowHidden.length; i++){
+            document.querySelector(this.state.idRowHidden[i]).style.display='block';
+        }
+    };
+    render(){
+        return (
+            <main className="main-container">
+                <section className="main-content">
+                    <h2 className="main-title">{this.props.secTitle}</h2>
+                    <BoxContainer boxNumber={[1,2]} />
+                    <BoxContainer boxNumber={[3,4]} />
+                    <BoxContainer boxNumber={[5,6]} />
+                    <BoxContainer boxNumber={[7,8]} />
+                    <BoxContainer boxNumber={[9,10]} />
+                    <a href="#" className="main-button" onClick={this.showBoxes}>
+                    Call to action
+                    </a>
+                </section>
+            </main>
+        )
+    }
+}
+class Menu extends React.Component {
+    state = {
+        idHeaderNavMobile: '.header-nav-mobile'
+    }
+    switchMenu = () => {
+        let tempStr = document.querySelector(this.state.idHeaderNavMobile).classList;
+        (tempStr.contains('displayBlock'))? tempStr.remove('displayBlock'):tempStr.add('displayBlock');
+    };
+    render(){
+        return(
+            <img src="img/menu.svg" alt="" className="header-menu" onClick = {this.switchMenu}/>
+        )
+    }
+}
+class Close extends React.Component {
+    state = {
+        idHeaderNavMobile: '.header-nav-mobile'
+    }
+    switchMenu = () => {
+        let tempStr = document.querySelector(this.state.idHeaderNavMobile).classList;
+        (tempStr.contains('displayBlock'))? tempStr.remove('displayBlock'):tempStr.add('displayBlock');
+    };
+    render(){
+        return(
+            <img src="img/close.svg" alt="close" title="icon designed by Zeke" className="header-closeImg" onClick = {this.switchMenu}/>
+        )
+    }
+}
+class Navlist extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render(){
+        return(
+            <ul className={this.props.class}>
+                <li>About</li>
+                <li>Works</li>
+                <li>Experiences</li>
+                <li>Contact me</li>
+            </ul>
+        )
+    }
+}
+class Container extends React.Component {
+    render(){
+        return(
+            <div className="container">
+                <header>
+                    <header className="header-head">
+                        <div className="header-logo">
+                            <span className="header-title">Zeke's website</span>
+                            <img className="header-logoImg" src="img/logo.svg"  />
+                        </div>
+                        <Menu />
+                        <nav className="header-nav-mobile displaySwitch">
+                            <Close />
+                            <Navlist class="header-ul-mobile" />
+                        </nav>
+                        <nav className="header-nav">
+                            <Navlist class="header-ul" />
+                        </nav>
+                    </header>
+                    <Slogan />
+                </header>
+                <MainContainer secTitle="Here is section title" />
+            </div>
+        )
+    }
+};
+ReactDOM.render(
+    <Container />,
+    document.getElementById('root')
+)
 //Pending issue. The width is separated with that in CSS, which means I need to modify both files if I change the width
 window.addEventListener("resize", function() {
-    let tempStr = document.querySelector(DOM.menuMobile).classList;
+    let tempStr = document.querySelector('.header-nav-mobile').classList;
     if (window.matchMedia("(min-width: 800px)").matches) {
     //Remove the class
     tempStr.remove('displayBlock')
